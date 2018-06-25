2f1f7c13700b342d4fae5a127bb0dd13013c8b89
Hamid Zare <dellydela@gmail.com>
2018-06-25 17:23:20 -0500

    added wifi direct and location permission helpers

--- www/js/index.js
+++ www/js/index.js
@@ -16,31 +16,118 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+'use strict';
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function ensureLocPerm() {
+  const diagnostic = cordova.plugins.diagnostic;
+  // diagnostic.getPermissionAuthorizationStatus(function(status) {
+  //   switch(status) {
+  //     case diagnostic.permissionStatus.GRANTED:
+  //       console.log('Permission granted to use the location');
+  //       break;
+  //     case diagnostic.permissionStatus.NOT_REQUESTED:
+  //       console.log('Permission to use the location has not been requested yet');
+  //       break;
+  //     case diagnostic.permissionStatus.DENIED:
+  //       console.log('Permission denied to use the location - ask again?');
+  //       break;
+  //     case diagnostic.permissionStatus.DENIED_ALWAYS:
+  //       console.log('Permission permanently denied to use the location - guess we won't be using it then!');
+  //       break;
+  //   }
+  // }, function(error) {
+  //   console.error('The following error occurred: '+error);
+  // }, cordova.plugins.diagnostic.permission.ACCESS_FINE_LOCATION);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+  diagnostic.isLocationAvailable(function(available) {
+    console.log('Location is ' + (available ? 'available' : 'not available'));
+    if(!available) { // either location is off or no access
+      diagnostic.requestLocationAuthorization(function(status) {
+        switch(status) {
+        case diagnostic.permissionStatus.NOT_REQUESTED:
+          console.log('Permission not requested');
+          break;
+        case diagnostic.permissionStatus.GRANTED:
+          console.log('Permission granted');
+          break;
+        case diagnostic.permissionStatus.DENIED:
+          console.log('Permission denied');
+          break;
+        case diagnostic.permissionStatus.DENIED_ALWAYS:
+          console.log('Permission permanently denied');
+          break;
+        }
+      }, function(error) {
+        console.error(error);
+      });
     }
+  }, function(error) {
+    console.error('The following error occurred: '+error);
+  });
+}
+
+let peerList = [],
+  wdNode;
+
+function getNode() {
+  const wd = cordova.plugins.wifi_direct;
+  return new Promise((accept, reject) => {
+    wd.getInstance(1, 1, 1, 1, 1, accept, reject);
+  });
+}
+
+function stopDiscovering() {
+  wdNode.stopDiscovering(console.log, console.error);
+}
+
+// checks for peers every 15 secs
+function startDiscovering() {
+  wdNode.startDiscovering(peers => {
+    // filter and save the robot peers
+    peerList = peers.filter(() => true);
+  }, console.error);
+}
+
+function connectPeer(target) {
+  return new Promise((accept, reject) => {
+    wdNode.connect(target, accept, reject);
+  });
+}
+
+
+var app = {
+  // Application Constructor
+  initialize: function() {
+    document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+  },
+
+  // deviceready Event Handler
+  //
+  // Bind any cordova events here. Common events are:
+  // 'pause', 'resume', etc.
+  onDeviceReady: function() {
+    this.receivedEvent('deviceready');
+    ensureLocPerm(); // async
+    getNode()
+      .then(node => {
+        wdNode = node;
+        return node;
+      })
+      .then(startDiscovering);
+  },
+
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
+
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+
+    console.log('Received Event: ' + id);
+  }
 };
 
 app.initialize();
