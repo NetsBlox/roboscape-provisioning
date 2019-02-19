3e2c813e3aacfd8f1ad0f0f46f704d26c27080fa
Hamid Zare <dellydela@gmail.com>
2019-02-18 19:12:21 -0600

    fixed critical issues with waitUntill helpers

--- www/js/utils.js
+++ www/js/utils.js
@@ -43,24 +43,54 @@ function waitUntil(fn, maxWait) {
   });
 };
 
-function waitUntilPromise(fn, maxWait=5000) {
-  var startTime = Date.now();
-  return new Promise((resolve, reject) => {
-    var check = async function() {
-      try {
-        let result = await fn(); // assuming the function also has a reasonable timeout of its own
-        resolve(result);
-      } catch (e) { // fn is not resolving yet;
-        if (Date.now()-startTime > maxWait) {
-          reject('timed out');
-        } else {
-          setTimeout(check, 100);
-        }
+// wait until the async function returns true
+async function waitUntilPromiseTF(fn, opts) {
+  opts = {
+    maxWait: 5000,
+    delayPerCheck: 300,
+    params: [],
+    ...opts, // WARN doesn't supported nested merging
+  };
+
+  const startTime = Date.now();
+
+  while (true) {
+    let rv = await fn(opts.params); // assuming the function also has a reasonable timeout of its own
+    if (rv === true) {
+      return true;
+    } 
+    if (Date.now()-startTime > opts.maxWait) {
+      throw new Error('timed out');
+    } else {
+      await sleep(opts.delayPerCheck); // WARN way slower than setTimeout
+    }
+    
+  }
+}
+
+// wait until the async function resolves
+async function waitUntilPromise(fn, opts) {
+  opts = {
+    maxWait: 5000,
+    delayPerCheck: 300,
+    params: [],
+    ...opts, // WARN doesn't supported nested merging
+  };
+  let startTime = Date.now();
+  let check = async () => {
+    try {
+      let result = await fn(); // assuming the function also has a reasonable timeout of its own
+      return result;
+    } catch (e) { // fn is not resolving yet;
+      if (Date.now()-startTime > opts.maxWait) {
+        throw new Error('timed out');
+      } else {
+        setTimeout(check, delayPerCheck);
       }
-    };
-    check();
-  });
-};
+    }
+  };
+  await check();
+}
 
 function sleep(duration) {
   console.debug('sleeping for', duration);
